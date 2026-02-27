9c729b0d60581cc8bd0e8473ec175a01c0661532
wrldbyalex <wrldbyalex@gmail.com>
2026-02-28 09:34:08 +1100

    adding information to page 2
    This page is teaching them about the basics of python.

--- app.py
+++ app.py
@@ -14,14 +14,19 @@ st.title("🐍 Python Learning Path")
 
 # Sidebar for organization
 st.sidebar.title("Course Map")
-the_page = st.sidebar.radio("Go to:", ["Home Page", "Variables", "Week 2", "Week 3", "Week 4"])
+the_page = st.sidebar.radio("Go to:", ["Home Page", "The basics", "Week 2", "Week 3", "Week 4"])
 
 if the_page == "Home Page":
     welcome()
 
-elif the_page == "Variables":
-    st.header("Week 1: Variables")
-    st.info("Coming soon: Learn how to store data!")
+elif the_page == "The basics":
+    st.header("Week 1: The Basics")
+    st.info("In this stage, you will learn about the absolute basics of Python, including syntax, variables, and simple data types. This is the foundation for your Python journey!")
+    st.write("Here is a simple example of Python code:")
+    st.code("print('Hello, World!)")
+    st.write("Lets break down this code:" \
+    "`print()` is a function that outputs text to the console." \
+    "`'Hello, World!'` is a string, represents text (normal writing). In this case, it will display the message 'Hello, World!' when the code is run.")
 
 elif the_page == "Week 2":
     st.header("Week 2: Data Types")
