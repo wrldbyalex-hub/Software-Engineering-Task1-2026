b02189e515503ebd261eb261cd279846fe41a263
wrldbyalex <wrldbyalex@gmail.com>
2026-03-31 14:08:49 +1100

    this wasn't pushed

--- utils/helpers.py
+++ utils/helpers.py
@@ -9,7 +9,7 @@ def show_home_content():
     chart_data = {"Python": 25, "Java": 21, "JavaScript": 8, "C#": 7}
     st.bar_chart(chart_data)
 
-    col1, col2 = st.columns(1) # columns help with the look and also are better for bigger screens 
+    col1, col2 = st.columns(2) # columns help with the look and also are better for bigger screens 
 
     with col1:
         st.subheader("What can you do with python?")
